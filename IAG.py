40997c61aa0a006148b664c26a68399e788143de
sagalpreet <sagalpreetsingh60@gmail.com>
2021-03-28 17:00:32 +0530

    Added IAG to be used in fetch

--- IAG.py
+++ IAG.py
@@ -1,9 +1,9 @@
 class IAG:
     def __init__(self, PC):
         self.PC = PC
-        self.muxControl = -1     ## 0 for incrementing 4         ## 1 for branch / jump instructions
+        self.PCSrc = -1     ## 0 for incrementing 4         ## 1 for branch / jump instructions
 
     def update(self, immediate = 0):
-        self.PC += 4 * (1 - self.muxControl) + 2 * immediate * (self.muxControl)
+        self.PC += 4 * (1 - self.PCSrc) + 2 * immediate * (self.PCSrc)
 
 ## PC_TEMP has to be stored in a variable
